51fd659d067c2ee823a244b63c4fa7c3c5b1118b
Skyler Kidd <f.s.kidd@gmail.com>
2016-05-05 20:29:15 -0700

    Update User model

--- app/models/Users.js
+++ app/models/Users.js
@@ -2,7 +2,29 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-    _id: String //email
+    _id: String, //email
+    password: String, //SHA hash, not string
+    settings: {
+        //alert preferences, etc.
+    },
+    phone_number: String,
+    // primary_numbers: [{
+    //     number: String
+    // }],
+    // secondary_numbers: [{
+    //     number: String
+    // }],
+    devices: [{
+        device_id: String, //Comes with device (randomly generated)
+        device_name: String, //Set by user
+        secret_key: String, //Would be given to device by server when registered
+        armed: Boolean
+    }],
+    alerts: [{
+        device_id: String,
+        time_stamp: Date,
+        picture: Image
+    }]
 });
 
 var User = mongoose.model('User', userSchema);
